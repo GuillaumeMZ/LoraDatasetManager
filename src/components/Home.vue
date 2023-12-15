bf7a3abb03c597f9ab3fe2fba3addf73565cca46
Guillaume M <58004135+GuillaumeMZ@users.noreply.github.com>
2023-12-15 16:12:50 +0100

    Initial commit: project setup

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="home">
+        <p>Home</p>
+    </div>
+</template>
+
+<style scoped>
+    div {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import PrimeVue from "primevue/config";
+import 'primevue/resources/themes/lara-dark-green/theme.css';
+
+import "./styles.css";
+import App from "./App.vue";
+import router from "./router";
+
+createApp(App)
+    .use(router)
+    .use(createPinia())
+    .use(PrimeVue)
+    .mount("#app");
