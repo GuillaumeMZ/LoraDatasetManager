b45c8ec6c459e9951fa27376e6fa9f7a28aae5fd
Guillaume M <58004135+GuillaumeMZ@users.noreply.github.com>
2023-12-18 18:46:12 +0100

    feat: display a text if the dataset doesn't contain any image

---
+++ src/components/DatasetGalleria.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+    import { ref } from "vue";
+
+    import Image from "primevue/image";
+    import Galleria from "primevue/galleria";
+
+    const activeIndex = ref();
+
+    const props = defineProps(["images"]);
+    
+</script>
+
+<template>
+    <Galleria v-model:activeIndex="activeIndex" :value="props.images" :numVisible="5" :showItemNavigators="true">
+        <template #item="slotProps">
+            <Image :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" preview />
+        </template>
+        <template #thumbnail="slotProps">
+            <img :src="slotProps.item.thumbnailImageSrc" :alt="slotProps.item.alt" />
+        </template>
+    </Galleria>
+</template>
+
+<style>
+    /* TODO: clean css */
+    .p-galleria {
+        width: 100%;
+        height: 100%;
+    }
+
+    .p-galleria-content {
+        width: 100%;
+        height: 100%;
+    }
+
+    .p-galleria-item-wrapper {
+        width: 100%;
+        flex-grow: 1;
+    }
+
+    .p-galleria-item-container {
+        width: 100%;
+        height: 100%;
+    }
+
+    .p-galleria-item {
+        width: 100%;
+        height: 100%;
+    }
+
+    .p-image {
+        width: 100%;
+        height: 100%;
+        position: relative;
+    }
+
+    img {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        object-fit: contain;
+    }
+
+    .p-galleria-item-nav {
+        z-index: 2;
+    }
+
+    .p-galleria-thumbnail-wrapper {
+        width: 100%;
+        height: 100px;
+    }
+
+    .p-galleria-thumbnail-container {
+        width: 100%;
+        height: 100%;
+    }
+
+    .p-galleria-thumbnail-item-content {
+        width: 140px;
+        height: 100px;
+        position: relative;
+    }
+
+    .p-image-action {
+        z-index: 1;
+    }
+</style>
--- src/pages/DatasetEditor.vue
+++ src/pages/DatasetEditor.vue
@@ -1,47 +1,47 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import { convertFileSrc } from "@tauri-apps/api/tauri";
+    import { computed, ref } from "vue";
 
-import Galleria from "primevue/galleria";
-import Image from "primevue/image";
+    import Sidebar from "primevue/sidebar";
 
-import DatasetEditorMenu from "../components/DatasetEditorMenu.vue";
-import { useDatasetStore } from "../stores/datasetStore";
-import { ValidItem } from "../types/dataset";
+    import DatasetEditorMenu from "../components/DatasetEditorMenu.vue";
+    import DatasetGalleria from "../components/DatasetGalleria.vue";
+    import FileList from "../components/FileList.vue";
+    import { useDatasetStore } from "../stores/datasetStore";
 
-const store = useDatasetStore();
+    import { convertFileSrc } from "@tauri-apps/api/tauri";
+    import { ValidItem } from "../types/dataset";
 
-const activeIndex = ref();
+    const sidebarVisible = ref(false); 
 
-const images = computed(() => {
-    const validItems = store.datasetItems.filter(item => "ValidItem" in item) as ValidItem[];
+    const store = useDatasetStore();
+
+    const images = computed(() => {
+        const validItems = store.datasetItems.filter(item => "ValidItem" in item) as ValidItem[];
     
-    return validItems.map(item => ({
-        itemImageSrc: convertFileSrc(item.ValidItem[0]),
-        thumbnailImageSrc: convertFileSrc(item.ValidItem[0]),
-        alt: "test",
-        title: "test"
-    }));
-});
-
+        return validItems.map(item => ({
+            itemImageSrc: convertFileSrc(item.ValidItem[0]),
+            thumbnailImageSrc: convertFileSrc(item.ValidItem[0]),
+            alt: "test",
+            title: "test"
+        }));
+    });
 </script>
 
 <template>
     <div id="root">
-        <DatasetEditorMenu />
-        <Galleria v-model:activeIndex="activeIndex" :value="images" :numVisible="5" :showItemNavigators="true">
-            <template #item="slotProps">
-                <Image :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" preview />
-            </template>
-            <template #thumbnail="slotProps">
-                <img :src="slotProps.item.thumbnailImageSrc" :alt="slotProps.item.alt" />
-            </template>
-        </Galleria>
+        <DatasetEditorMenu :showSidepanel="() => {sidebarVisible = true;}" />
+        <Sidebar v-model:visible="sidebarVisible" header="Files">
+            <FileList/>
+        </Sidebar>
+        <DatasetGalleria v-if="images.length !== 0" :images="images"/>
+        <div id="no-images" v-else>
+            <p>It looks like there is no image in the dataset you selected... 😔</p>
+            <router-link to="/">Go back to the main menu</router-link>
+        </div>
     </div>
 </template>
 
 <style>
-    /* TODO: clean the css */
     #root {
         width: 100vw;
         height: 100vh;
@@ -49,67 +49,12 @@ const images = computed(() => {
         flex-direction: column;
     }
 
-    .p-galleria {
+    #no-images {
         width: 100%;
         height: 100%;
-    }
-
-    .p-galleria-content {
-        width: 100%;
-        height: 100%;
-    }
-
-    .p-galleria-item-wrapper {
-        width: 100%;
-        flex-grow: 1;
-    }
-
-    .p-galleria-item-container {
-        width: 100%;
-        height: 100%;
-    }
-
-    .p-galleria-item {
-        width: 100%;
-        height: 100%;
-    }
-
-    .p-image {
-        width: 100%;
-        height: 100%;
-        position: relative;
-    }
-
-    img {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        object-fit: contain;
-    }
-
-    .p-galleria-item-nav {
-        z-index: 2;
-    }
-
-    .p-galleria-thumbnail-wrapper {
-        width: 100%;
-        height: 100px;
-    }
-
-    .p-galleria-thumbnail-container {
-        width: 100%;
-        height: 100%;
-    }
-
-    .p-galleria-thumbnail-item-content {
-        width: 140px;
-        height: 100px;
-        position: relative;
-    }
-
-    .p-image-action {
-        z-index: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
 </style>
