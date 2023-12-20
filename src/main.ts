35477e367340226ada350a52e78bd6a3d4cf33fc
Guillaume M <58004135+GuillaumeMZ@users.noreply.github.com>
2023-12-21 00:27:02 +0100

    feat: add toast area in App.vue

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
 import 'primevue/resources/themes/lara-dark-green/theme.css';
+import ToastService from "primevue/toastservice";
 
 import "./styles.scss";
 import App from "./App.vue";
@@ -11,4 +12,5 @@ createApp(App)
     .use(router)
     .use(createPinia())
     .use(PrimeVue)
+    .use(ToastService)
     .mount("#app");
